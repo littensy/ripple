c18e8873eec43f21537a9592352111232c178b21
Richard <richerd.rdc@gmail.com>
2024-01-15 18:20:47 -0800

    style: improve formatting

--- scripts/analyze.sh
+++ scripts/analyze.sh
@@ -2,6 +2,11 @@ curl -o scripts/roblox.d.lua https://raw.githubusercontent.com/JohnnyMorganz/lua
 
 rojo sourcemap test.project.json -o sourcemap.json
 
-luau-lsp analyze --settings=scripts/analyze-settings.json --defs=scripts/roblox.d.lua --defs=scripts/testez.d.lua --base-luaurc=.luaurc --sourcemap=sourcemap.json src
+luau-lsp analyze \
+	--settings=scripts/analyze-settings.json \
+	--defs=scripts/roblox.d.lua \
+	--defs=scripts/testez.d.lua \
+	--base-luaurc=.luaurc \
+	--sourcemap=sourcemap.json src
 
 rm scripts/roblox.d.lua
