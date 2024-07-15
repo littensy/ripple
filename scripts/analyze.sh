acb966dc83dda6d11381af5ce6543b7be573f11f
Christopher Buss <christopher.buss+github@protonmail.com>
2024-07-15 20:41:07 +0100

    chore: convert `*.lua` to `*.luau` (#5)

--- scripts/analyze.sh
+++ scripts/analyze.sh
@@ -1,12 +1,12 @@
-curl -o scripts/roblox.d.lua https://raw.githubusercontent.com/JohnnyMorganz/luau-lsp/main/scripts/globalTypes.d.lua
+curl -o scripts/roblox.d.lua https://raw.githubusercontent.com/JohnnyMorganz/luau-lsp/main/scripts/globalTypes.d.luau
 
 rojo sourcemap test.project.json -o sourcemap.json
 
 luau-lsp analyze \
 	--settings=scripts/analyze-settings.json \
-	--defs=scripts/roblox.d.lua \
-	--defs=scripts/testez.d.lua \
+	--defs=scripts/roblox.d.luau \
+	--defs=scripts/testez.d.luau \
 	--base-luaurc=.luaurc \
 	--sourcemap=sourcemap.json src
 
-rm scripts/roblox.d.lua
+rm scripts/roblox.d.luau
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,5 @@
 rojo build test.project.json -o test.rbxl
 
-run-in-roblox --place test.rbxl --script scripts/run-tests.server.lua
+run-in-roblox --place test.rbxl --script scripts/run-tests.server.luau
 
 rm test.rbxl
